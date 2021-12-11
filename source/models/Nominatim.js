5ca80d0471f41974a070ea19a1a860899d099ca9
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2021-12-11 11:20:46 -0300

    UPDATED - API Nominatim adaptada

--- source/models/Nominatim.js
+++ source/models/Nominatim.js
@@ -4,16 +4,38 @@ const baseUrl = 'https://nominatim.openstreetmap.org';
 const Message = require('../utils/Message');
 
 class Nominatim {
-    static async findAddressByCoordinates(lat, long) {
-        try{
-            const result = await axios.get(`${baseUrl}/reverse?lat=${lat}&lon=${long}`);
-            
-            return {success: true, address: result.data};
-        } catch(e) {
+    static async findAddressesByPosition(position) {
+        try {
+            let coords;
+            if (position.includes('POLYGON')) {
+                coords = position.split('((')[1].replace('))', '').split(',');
+            } else {
+                coords = position.split('(')[1].replace(')', '').split(',')
+            }
+
+            const requests = coords.map(coord => {
+                coord = coord.split(' ');
+
+                return axios.get(`${baseUrl}/reverse?format=jsonv2&lat=${coord[0]}&lon=${coord[1]}`)
+            });
+
+            const result = await Promise.all(requests);
+
+            const addresses = [];
+            for (const res of result) {
+                if (!!res.data.display_name) {
+                    addresses.push({ address: res.data.display_name });
+                } else {
+                    throw new Error('Nominatim (OMS) - ' + res.data.error);
+                }
+            }
+
+            return { success: true, addresses };
+        } catch (e) {
             Message.warning(e);
-            return {success: false, message: 'Houve um erro ao recuperar o endereço da coordenada!'};
+            return { success: false, message: 'Houve um erro ao recuperar os endereços das coordenadas!' };
         }
     }
 }
 
-module.exports = { Nominatim };
+module.exports = Nominatim;
