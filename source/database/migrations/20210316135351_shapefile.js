f6b202d487d6f60606cbcc250dc055e01476279a
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-01-05 16:22:55 -0300

    UPDATED - Adição de atributo path à tabela Shapefile

--- source/database/migrations/20210316135351_shapefile.js
+++ source/database/migrations/20210316135351_shapefile.js
@@ -6,7 +6,8 @@ exports.up = async function (knex) {
     await knex.schema.createTable(tableName, function (table) {
         table.increments('id').notNullable().primary();
         table.string('title', 50).notNullable().unique();
-        table.string('uri', 100).notNullable();
+        table.string('uri', 80).notNullable();
+        table.string('path', 200).notNullable();
         table.string('desc', 100);
         table.integer('added_by').unsigned().notNullable().references('id').inTable('user');
         table.timestamps(false, true);
