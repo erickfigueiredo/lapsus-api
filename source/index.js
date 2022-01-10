0ce2ad221dd1985e6d684e874cd51677a422c969
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-01-10 15:40:47 -0300

    UPDATED - Exposição de diretórios de arquivos

--- source/index.js
+++ source/index.js
@@ -10,9 +10,13 @@ const helmet = require('helmet');
 const app = express();
 const router = require('./routes/routes');
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({origin: true, credentials: true}));
+//app.use(cors({ origin: 'http://localhost:8080' }));
 
-//app.use('/shapefiles', express.static('../upload/shapefiles'));
+// Diretórios estáticos expostos
+app.use('/legend', express.static('../upload/legend_items'));
+app.use('/shapefiles', express.static('../upload/shapefiles'));
+app.use('/annexes', express.static('../upload/annexes'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
