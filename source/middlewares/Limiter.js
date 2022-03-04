438212729a266bb85bd01fbe38a8672287e43bfa
Erick Figueiredo <31293103+erickfigueiredo@users.noreply.github.com>
2022-03-04 17:06:40 -0300

    UPDATED - Adição de parametros no Limitador de Requisições

--- source/middlewares/Limiter.js
+++ source/middlewares/Limiter.js
@@ -1,9 +1,12 @@
 const rateLimit = require('express-rate-limit');
 
 const requestLimiter = rateLimit({
-    windowMs: (parseInt(process.env.RATE_MINUTE) || 1) * 60 * 1000, // 30 segundos
+    windowMs: (parseInt(process.env.RATE_MINUTE) || 0.5) * 60 * 1000, // tempo medido em minutos
     max: (parseInt(process.env.RATE_LIMIT) || 30), // Limite de requisições
-    handler(req, res) {
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler(req, res, next) {
+        if(req.path.includes('/legend')) return next();
         return res.status(429).send({ success: false, message: 'Você excedeu o limite de requisições, tente novamente mais tarde!' })
     }
 });
